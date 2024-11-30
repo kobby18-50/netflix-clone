ba815f4423f25209fde2cf9bef3d05cb5f65d8c8
Kwabena <79258511+kobby18-50@users.noreply.github.com>
2024-11-30 11:15:55 +0000

    updates on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return ( 
         <footer className="mt-10 flex items-center justify-center bg-slate-300 gap-x-1">
-           <span> Designed by</span> <Link to={'https://ekwabena.netlify.app/'} className='font-semibold'> Kwabena</Link>
+           <span> Developed by</span> <Link to={'https://ekwabena.netlify.app/'} className='font-semibold'> Kwabena</Link>
         </footer>
      );
 }
